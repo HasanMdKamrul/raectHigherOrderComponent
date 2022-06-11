2f4dd3639b2ccea8ac2a3e12880b000878ed94eb
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-06-11 02:05:03 +0200

    Back to the previous function again

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,32 +1,21 @@
-import React from 'react';
 import ClickHover from './ClickHover';
 import ThemeContext from './Contexts/ThemeContext';
 import Intermediate from './Intermediate';
 
-export default class Content extends React.Component {
+export default function Content(){
 
-    componentDidMount() {
-        const {theme,switchTheme} = this.context;
-        console.log(theme);
-        console.log(switchTheme);
-    }
-
-    render() {
-        return (
-            <div>
-                 <h1>This is Content</h1>
-                 <Intermediate>
-                     {(count,increment) => (
-                         <ThemeContext.Consumer>
-                             {({theme, switchTheme}) => (
-                                 <ClickHover count={count} increment={increment} theme={theme} switchTheme={switchTheme}/>
-                             )}
-                         </ThemeContext.Consumer>
-                     )}
-                 </Intermediate>
-            </div>
-         )
-    }
+    return (
+       <div>
+            <h1>This is Content</h1>
+            <Intermediate>
+                {(count,increment) => (
+                    <ThemeContext.Consumer>
+                        {({theme, switchTheme}) => (
+                            <ClickHover count={count} increment={increment} theme={theme} switchTheme={switchTheme}/>
+                        )}
+                    </ThemeContext.Consumer>
+                )}
+            </Intermediate>
+       </div>
+    )
 }
-
-Content.contextType = ThemeContext;
