6d80ede9246755e1a5692a3d4874131e4818e2a7
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-06-11 02:09:17 +0200

    Solving the same problem using useContext hook in function

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,19 +1,16 @@
+import { useContext } from 'react';
 import ClickHover from './ClickHover';
 import ThemeContext from './Contexts/ThemeContext';
 import Intermediate from './Intermediate';
 
 export default function Content(){
-
+    const {theme,switchTheme} = useContext(ThemeContext);
     return (
        <div>
             <h1>This is Content</h1>
             <Intermediate>
                 {(count,increment) => (
-                    <ThemeContext.Consumer>
-                        {({theme, switchTheme}) => (
-                            <ClickHover count={count} increment={increment} theme={theme} switchTheme={switchTheme}/>
-                        )}
-                    </ThemeContext.Consumer>
+                    <ClickHover count={count} increment={increment} theme={theme} switchTheme={switchTheme}/>
                 )}
             </Intermediate>
        </div>
